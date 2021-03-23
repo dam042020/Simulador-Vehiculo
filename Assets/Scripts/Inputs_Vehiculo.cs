1c3fd6faec0bd5058583f162e991ab716a936807
Sergio Ezquerro <dam04.2020.jesuitas@gmail.com>
2021-03-23 03:55:08 +0100

    Initial commit

---
+++ Assets/Scripts/Inputs_Vehiculo.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs_Vehiculo : MonoBehaviour
+{
+    public float AccelerationOnInput = 0.1f;
+    public float DecelerationRatio = 0.1f;
+    public float MaxSpeed = 20f;
+    public float Speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Variables
+        float zMovement = 0;
+        float xRotation = 0;
+
+        /*
+        // Usar para evitar inputs cuando el coche no toquue el suelo
+        if (transform.rotation.eulerAngles.y < 10)
+        {
+        */
+            // Controles
+            if (Input.GetKey("w"))
+            {
+                zMovement += 1;
+            }
+
+            if (Input.GetKey("s"))
+            {
+                zMovement -= 1;
+            }
+
+            if (Input.GetKey("a"))
+            {
+                xRotation += 1;
+            }
+
+            if (Input.GetKey("d"))
+            {
+                xRotation -= 1;
+            }
+        /*
+        }
+        */
+
+        // Velocidad
+        Speed += AccelerationOnInput * zMovement - (Speed * DecelerationRatio);
+        if (Speed < -MaxSpeed)
+        {
+            Speed = -MaxSpeed;
+        }
+        else if (Speed > MaxSpeed)
+        {
+            Speed = MaxSpeed;
+        }
+        if (Mathf.Abs(Speed) < 0.05f)
+        {
+            Speed = 0;
+        }
+
+        // Movimiento
+        Vector3 movement = new Vector3(0, 0, Speed);
+        movement = transform.rotation * movement;
+        movement.y = 0; // Elimina cambios de altura
+        movement.Normalize();
+        transform.Translate(Mathf.Abs(Speed) * movement * Time.deltaTime, Space.World);
+
+        // Rotacion
+        if (Speed != 0)
+        {
+            transform.RotateAround(transform.position, new Vector3(0, 1, 0), Mathf.Abs(Speed) * xRotation * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Seguir_Vehiculo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seguir_Vehiculo : MonoBehaviour
+{
+    // camera will follow this object
+    public Transform Target;
+    //camera transform
+    public Transform camTransform;
+    // offset between camera and target
+    public Vector3 Offset;
+    // change this value to get desired smoothness
+    public float SmoothTime = 0.1f;
+
+    // This value will change at the runtime depending on target movement. Initialize with zero vector.
+    private Vector3 velocity = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update position
+        Vector3 targetPosition = Target.position + Offset;
+        camTransform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
+
+        // update rotation
+        transform.LookAt(Target);
+    }
+}
